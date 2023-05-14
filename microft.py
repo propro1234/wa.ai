d8c346686f8b428f20f2654db32e238e4689a1cf
propro1234 <132676622+propro1234@users.noreply.github.com>
2023-05-14 20:59:40 +0530

    Add files via upload

--- microft.py
+++ microft.py
@@ -1,14 +1,14 @@
 import openai
 import os
 import sys
-from flask import Flask, request, app
+from flask import Flask, request, app, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
-# acc_sid = os.getenv('TWILIO_ACCOUNT_SID')
-# auth_tok = os.environ.get('TWILIO_AUTH_TOKEN')
+# # acc_sid = os.getenv('TWILIO_ACCOUNT_SID')
+# # auth_tok = os.environ.get('TWILIO_AUTH_TOKEN')
 
 def user_Q(ureq):
     req = openai.Completion.create(
@@ -19,24 +19,27 @@ def user_Q(ureq):
         frequency_penalty = 0.5,
         presence_penalty = 0
     )
-    res = req['choices'][0]['text']
-    return res.decode('utf-8')
-# @app.route("/")
+    return req['choices'][0]['text'].strip()
+@app.route("/")
+def index():
+    return "hello world"
 # def index():
 #    return render_template("index.html")
-@app.route("/chat",methods=['POST','GET'])
-def chat():
-    in_que = request.values.get('BODY', '')
 
+@app.route("/chat",methods=['POST'])
+def chat():
+    in_que = request.form.get('Body')
+    # return render_template("index.html",in_msg = f"message from whatsapp: {in_que}")
     ans = user_Q(in_que)
 
     msg = MessagingResponse()# creates an object of MessagingResponse
-    wa_msg = msg.message()# creates an instance of that object
-    wa_msg.body(ans)#with that object we keeping the ans on the body
+    # msg.message(f"You said: {in_que}")# creates an instance of that object
+    msg.message(ans)
+    # wa_msg.body(ans)#with that object we keeping the ans on the body
     
-    return msg #because of the framework can only detect/read the json object
+    return str(msg) #because of the framework can only detect/read the json object
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False,port=8085)
+    app.run(debug=True)
 
