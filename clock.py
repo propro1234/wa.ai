48c0dcce690038f49c3a13e0e19ee93870d82414
propro1234 <132676622+propro1234@users.noreply.github.com>
2023-05-14 21:09:01 +0530

    Delete clock.py

--- clock.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from microft import chat
-
-sched = BlockingScheduler()
-
-sched = add_job(chat, 'interval', seconds=0)
-
-sched.start()
